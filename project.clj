6deac35a7415a75d600261e2e54207933cd8f92a
Jon Eisen <jon@metricfeat.com>
2015-08-10 16:11:56 -0600

    Fix release in project clj

--- project.clj
+++ project.clj
@@ -1,26 +1,19 @@
-(defproject com.rafflecopter/relyq "0.2.0"
+(defproject com.rafflecopter/relyq "0.2.1-SNAPSHOT"
   :description "Implementation of relyq, a reliable redis-based queue"
   :url "http://github.com/Rafflecopter/clj-relyq"
   :license {:name "MIT"
             :url "http://github.com/Rafflecopter/clj-relyq/blob/master/LICENSE"}
   :scm {:name "git"
         :url "https://github.com/Rafflecopter/clj-relyq"}
-  :deploy-repositories [["clojars" {:creds :gpg}]]
+  :deploy-repositories [["releases" :clojars]]
 
   :resource-paths ["src/lua"]
   :source-paths ["src/clj"]
-
-  :release-tasks [["vcs" "assert-committed"]
-                  ["change" "version"
-                   "leiningen.release/bump-version" "release"]
-                  ["vcs" "commit"]
-                  ["vcs" "tag"]
-                  ["deploy" "clojars"]]
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.taoensso/carmine "2.11.1"]
                  [org.clojure/data.json "0.2.6"]
-                 [com.rafflecopter/qb "0.2.0"]]
+                 [com.rafflecopter/qb "0.2.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]]}})
